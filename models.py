f0f4b72296f423cd17958764c12b83d5274fda22
marzziinn-dev <marzziinn@gmail.com>
2026-02-21 23:18:57 -0300

    Atualizar o models.py

--- models.py
+++ models.py
@@ -4,6 +4,7 @@ from database import Base
 
 class User(Base):
     __tablename__ = "users"
+
     id = Column(Integer, primary_key=True)
     email = Column(String, unique=True)
     password = Column(String)
@@ -14,10 +15,12 @@ class User(Base):
 
 class Transaction(Base):
     __tablename__ = "transactions"
+
     id = Column(Integer, primary_key=True)
     user_id = Column(Integer, ForeignKey("users.id"))
-    transaction_id = Column(String)
-    amount = Column(Integer)
-    status = Column(String)
-    type = Column(String)
+    transaction_id = Column(String)  # ID da transação na Promisse
+    amount = Column(Integer)  # Valor original em centavos (antes da taxa)
+    final_amount = Column(Integer, default=0)  # Valor líquido após taxa (em centavos)
+    status = Column(String)  # pending, approved, rejected
+    type = Column(String)  # deposit, withdraw, transfer_in, transfer_out
     created_at = Column(DateTime(timezone=True), server_default=func.now())
